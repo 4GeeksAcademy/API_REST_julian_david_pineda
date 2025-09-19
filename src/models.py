5c6f2c2447508f31b784696feb554a235b06ee89
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-09-19 22:34:35 +0000

    Tabla Planets Ok :tada:, configuration routes.py

--- src/models.py
+++ src/models.py
@@ -17,3 +17,35 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(), nullable=False)
+    diameter: Mapped[str] = mapped_column(String(100), nullable=False)
+    rotation_period: Mapped[str] = mapped_column(String(100), nullable=False)
+    gravity: Mapped[str] = mapped_column(String(100), nullable=False)
+    climate: Mapped[str] = mapped_column(String(100), nullable=False)
+    orbital_period: Mapped[str] = mapped_column(String(100), nullable=False)
+    population: Mapped[str] = mapped_column(String(100), nullable=False)
+    surface_water: Mapped[str] = mapped_column(String(100), nullable=False)
+    terrain: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "gravity": self.gravity,
+            "climate": self.climate,
+            "orbital_period": self.orbital_period,
+            "population": self.population,
+            "surface_water": self.surface_water,
+            "terrain": self.terrain
+        }
+    
+    
+
+
+
+ 
