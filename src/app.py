c7178a7e75f6b95f3670168b96879ecc3c66adfd
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-09-22 23:12:37 +0000

    Endpoint get_all_user ok :tada:

--- src/app.py
+++ src/app.py
@@ -9,7 +9,10 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+from routes import api
 #from models import Person
+
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -23,8 +26,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 MIGRATE = Migrate(app, db)
 db.init_app(app)
+
 CORS(app)
 setup_admin(app)
+
+
+app.register_blueprint(api, url_prefix='/api')
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
--- src/routes.py
+++ src/routes.py
@@ -2,13 +2,23 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Planets, People
-from api.utils import generate_sitemap, APIException
+from models import db, User, Planets, People
+from utils import generate_sitemap, APIException
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
+
+@api.route('/users', methods=['GET'])
+def get_all_user():
+    try: 
+        users = User.query.all()
+        return jsonify([user.serialize() for user in users]), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
 
